c5e1fb458b4839659e0c6ea062a828032f8bd804
kamleong <kamleong@users.noreply.github.com>
2021-07-02 22:17:03 +0800

    Update powershell_vb.bat

--- powershell_vb.bat
+++ powershell_vb.bat
@@ -1,1 +1,2 @@
+@rem cmd.exe "/c" "powershell.exe -noprofile -command ""Add-Type -ReferencedAssemblies System.Windows.Forms -Path """ + Environment.CurrentDirectory.Replace("\","\\") + "\\waitCursor.vb"" ; [myMainClass]::waitCursor(" + timeOut.ToString() + ") "
 powershell.exe -noprofile -command "Add-Type -ReferencedAssemblies System.Windows.Forms -Path \"waitCursor.vb\"; [myMainClass]::waitCursor(60)"
