361ee96748c958d162dfd14c1ce7f049d055e590
kamleong <kamleong@users.noreply.github.com>
2021-07-02 22:15:45 +0800

    Update powershell_cs.bat

--- powershell_cs.bat
+++ powershell_cs.bat
@@ -1,2 +1,2 @@
-@rem "/c powershell.exe -noprofile -command ""Add-Type -ReferencedAssemblies System.Windows.Forms -Path """ + Environment.CurrentDirectory.Replace("\","\\") + "\\waitCursor.cs"" ; [myClass]::waitCursor(" + timeOut.ToString() + ") "
+@rem cmd.exe "/c powershell.exe -noprofile -command ""Add-Type -ReferencedAssemblies System.Windows.Forms -Path """ + Environment.CurrentDirectory.Replace("\","\\") + "\\waitCursor.cs"" ; [myClass]::waitCursor(" + timeOut.ToString() + ") "
 powershell.exe -noprofile -command "Add-Type -ReferencedAssemblies System.Windows.Forms -Path \"B:\\waitCursor.cs\"; [myClass]::waitCursor(60)"
